28f9701e408e45769df6fb4cd65def4b405babb8
Yosuke Mizuguchi <mizuguchi.yosuke.fcp@gmail.com>
2020-01-12 23:12:45 +0900

    fix

--- abc/abc151/B - Achieve the Goal.rb
+++ abc/abc151/B - Achieve the Goal.rb
@@ -1,35 +1,5 @@
 n, k, m = gets.split.map(&:to_i)
 scores = gets.split.map(&:to_i)
-target_point = n * m
-
-# n科目、k点満点、目標m点
-# scoresが各試験の点数
-# n * m が必須合計点
-
-current_total = 0
-scores.each { |i| current_total += i }
-
-needed_score = target_score - current_total
-
-p n
-p k
-p m
-p scores
-p target_score
-p current_total
-p needed_score
-
-if needed_score > k
-  puts "-1"
-elsif needed_score <= 0
-  puts "0"
-else
-  puts needed_score
-end
-
-
-n, k, m = gets.split.map(&:to_i)
-scores = gets.split.map(&:to_i)
 
 target_score = n * m
 
