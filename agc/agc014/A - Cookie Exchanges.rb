54163fe680a9b7711315dcfe3282d11055e76630
Yosuke Mizuguchi <mizuguchi.yosuke.fcp@gmail.com>
2020-06-25 01:42:32 +0900

    agc014 - a

---
+++ agc/agc014/A - Cookie Exchanges.rb
@@ -0,0 +1,17 @@
+a, b, c = gets.split.map(&:to_i)
+
+1000.times do |i|
+  unless a.even? && b.even? && c.even?
+    puts i
+    exit
+  end
+
+  a2 = (b + c) / 2
+  b2 = (c + a) / 2
+  c2 = (a + b) / 2
+  a = a2
+  b = b2
+  c = c2
+end
+
+puts(-1)
