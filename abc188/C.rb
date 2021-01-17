f5fbef84e6257e657c94c6751bd29e843012a6da
Yosuke Mizuguchi <mizuguchi.yosuke.fcp@gmail.com>
2021-01-17 16:31:14 +0900

    abc188 - c

--- abc188/C.rb
+++ abc188/C.rb
@@ -1,2 +1,9 @@
-# gets.split.map(&:to_i)
-# readlines.map(&:chomp!).map { |e| e.split.map(&:to_i) }
+n, a = readlines.map(&:chomp).map { |e| e.split.map(&:to_i) }
+n = n.first
+num = 2**n
+
+max1 = a[0..(num/2-1)].max
+max2 = a[(num/2)..].max
+
+second =  [max1, max2].min
+puts a.index(second) + 1
