db800a4be1981db915b255a8271a6526249c5233
Yosuke Mizuguchi <mizuguchi.yosuke.fcp@gmail.com>
2022-03-07 00:39:47 +0900

    bundle update

--- Gemfile
+++ Gemfile
@@ -7,3 +7,4 @@ git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 # Automatically create workspace and tests for AtCoder contest
 gem 'green_day'
 gem 'pry'
+gem 'rspec'
---
+++ past201912-open/spec/D_spec.rb
@@ -0,0 +1,10 @@
+require 'rspec'
+
+RSpec.describe 'test' do
+  it 'test with "6\n1\n5\n6\n3\n2\n6"' do
+    io = IO.popen("ruby past201912-open/D.rb", "w+")
+    io.puts("6\n1\n5\n6\n3\n2\n6")
+    io.close_write
+    expect(io.readlines.join).to eq("6 4\n")
+  end
+end
