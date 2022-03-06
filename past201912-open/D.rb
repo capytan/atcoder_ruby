ce41a0237b7fabd889ae9d4342f8fc196adc5e2a
Yosuke Mizuguchi <mizuguchi.yosuke.fcp@gmail.com>
2022-03-07 01:29:38 +0900

    TLE: past2019020-open - D

--- past201912-open/D.rb
+++ past201912-open/D.rb
@@ -1,2 +1,14 @@
-a = gets.split.map(&:to_i)
-p a
+# TLE: 2207 ms, 27660 KB
+n = gets.to_i
+a = readlines.map(&:to_i)
+b = []
+
+1.upto n do |i|
+  b << a.count(i)
+end
+
+if b.all?(1)
+  puts 'Correct'
+else
+  puts "#{(b.index(2) + 1)} #{(b.index(0) + 1)}"
+end
