94af81ea9be1a64277e1d035c316a214619053ff
Yosuke Mizuguchi <mizuguchi.yosuke.fcp@gmail.com>
2022-05-05 00:09:14 +0900

    Refactor: ABC249 - B

--- abc249/B.rb
+++ abc249/B.rb
@@ -1,15 +1,17 @@
 s = gets.chomp
 
-if s != s.upcase
-  if s != s.downcase
-    if s.length == s.split("").uniq.length
-      puts 'Yes'
-    else
-      puts 'No'
-    end
-  else
-    puts 'No'
-  end
+if s == s.upcase
+  puts 'No'
+  exit
+end
+
+if s == s.downcase
+  puts 'No'
+  exit
+end
+
+if s.length == s.split("").uniq.length
+  puts 'Yes'
 else
   puts 'No'
 end
